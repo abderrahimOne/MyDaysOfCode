e903385a65046df22278f504f18d239c5cce349b
abderrahim101 <abderrahimaneddam1@gmail.com>
2023-01-25 11:27:54 +0100

    Day 4

--- main.js
+++ main.js
@@ -1,36 +1,17 @@
-let vid = document.querySelector(".viewer");
-let tog = document.querySelector(".toggle");
-let vts = document.querySelector('[name="playbackRate"]');
-let vol = document.querySelector('[name="volume"]');
-const progress = document.querySelector(".progress");
-const progressBar = document.querySelector(".progress__filled");
-tog.addEventListener("click", () => {
-  if (tog.id === "play") {
-    tog.id = "pause";
-    vid.play();
+const pressed = [];
+const secretCode = "wesbos";
+
+window.addEventListener("keyup", (e) => {
+  console.log(e.key);
+  if (e.key === "Backspace") {
+    pressed.splice(-1, 1);
   } else {
-    tog.id = "play";
-    tog.textContent = "❚❚";
-    vid.pause();
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join("").includes(secretCode)) {
+      console.log("DING DING!");
+      cornify_add();
+    }
   }
-});
-
-vid.addEventListener("timeupdate", () => {
-  const percent = (vid.currentTime / vid.duration) * 100;
-  progressBar.style.flexBasis = `${percent}%`;
-});
-vts.addEventListener("change", () => {
-  vid.playbackRate = parseInt(vts.value);
-});
-vol.addEventListener("change", () => {
-  vid.volume = parseInt(vol.value);
-});
-
-document.querySelector('[data-skip="-10"]').addEventListener("click", (e) => {
-  vid.currentTime =
-    vid.currentTime + parseFloat(e.target.getAttribute("data-skip"));
-});
-document.querySelector('[data-skip="25"]').addEventListener("click", (e) => {
-  vid.currentTime =
-    vid.currentTime + parseFloat(e.target.getAttribute("data-skip"));
+  console.log(pressed);
 });
